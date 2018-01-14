4492dc2e10175028e4c507e697f26f0cbe511bbd
Zapel <zapel176@ukr.net>
2018-01-14 11:21:06 +0200

    10

--- addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/GroupCreationTests.java
@@ -5,7 +5,9 @@ import net.ukr.ptf.addressbook.model.Groups;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -17,11 +19,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-        list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-        list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
@@ -48,12 +54,12 @@ public class GroupCreationTests extends TestBase {
         assertThat(after.size(), equalTo(before.size()));
     }
 
-    @Test
+    @Test (enabled=false)
     public void testCurrentDir() throws IOException {
         File currentDir = new File(".");
         System.out.println(currentDir.getCanonicalPath());
         System.out.println();
-        File photo = new File("src/test/resources/getImage.jpg");
+        File photo = new File("src/test/resources/groups.csv");
         System.out.println(photo.getCanonicalPath());
         System.out.println(photo.exists());
     }
