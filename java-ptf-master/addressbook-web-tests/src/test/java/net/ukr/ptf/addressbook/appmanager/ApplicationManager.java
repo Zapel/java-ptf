efa2245d9bacef916be065dbfd9be78427a3d6d2
Zapel <zapel176@ukr.net>
2018-01-21 23:14:59 +0200

    lesson78
    Смотреть, не работает

---
+++ java-ptf-master/addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,67 @@
+package net.ukr.ptf.addressbook.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private String browser;
+    private DbHelper DbHelper;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        DbHelper = new DbHelper();
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+        sessionHelper = new SessionHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        groupHelper = new GroupHelper(wd);
+        sessionHelper.login (properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public GroupHelper group() {
+        return groupHelper;
+    }
+
+    public NavigationHelper goTo() {
+        return navigationHelper;
+    }
+
+    public DbHelper db() {
+        return DbHelper;
+    }
+}
---
+++ sandbox/src/main/java/net/ukr/ptf/mantis/ukr/zapel/sandbox/Collections.java
@@ -0,0 +1,11 @@
+package net.ukr.ptf.mantis.ukr.zapel.sandbox;
+
+public class Collections {
+    public static void main(String args[]) {
+        String[] langs = {"java", "C#", "Python", "PHP"};
+        for (String l : langs) {
+            System.out.println(l);
+            }
+        }
+    }
+
