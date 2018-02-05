da2e4bfdb4a606910079f09c32e60cbb986eca41
Zapel <zapel176@ukr.net>
2018-02-05 23:53:08 +0200

    lesson84
    SOAP - simple

---
+++ soap-sample/src/test/java/net/ukr/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,22 @@
+package net.ukr.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("94.154.238.126");
+        assertEquals(geoIP.getCountryCode(), "UKR");
+    }
+
+    @Test
+    public void testInvalidIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("94.154.238.xxx");
+        assertEquals(geoIP.getCountryCode(), "UKR");
+    }
+}
