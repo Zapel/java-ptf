c15352ddfe2f1ba2a9cb6fc2ab956f6568b00eab
Zapel <zapel176@ukr.net>
2018-02-13 23:23:54 +0200

    home4(lesson23)
    TastBaseContact создал общий класс для контактов

--- addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/ContactCreationTests.java
@@ -1,83 +1,16 @@
 package net.ukr.ptf.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class ContactCreationTests {
-    ChromeDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/index.php");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+public class ContactCreationTests extends TestBaseContact {
 
     @Test
     public void testContactCreation() {
+        gotoHomePage();
         initContactCreation();
         fillContactForm(new ContactData("Oleg", "Lazeba", "Kiev", "zapel176@ukr.net"));
         submitContactCreation();
         returnToHomePage();
     }
 
-    private void returnToHomePage() {
-        wd.findElement(By.id("content")).click();
-    }
-
-    private void submitContactCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    private void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactData.getHome());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    private void initContactCreation() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod
-
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
---
+++ addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/zapel.java
@@ -0,0 +1,46 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class zapel {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void zapel() {
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.id("search-az")).click();
+        wd.findElement(By.linkText("home")).click();
+        wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img")).click();
+        wd.findElement(By.xpath("//div[@id='content']/form[2]/input[2]")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
