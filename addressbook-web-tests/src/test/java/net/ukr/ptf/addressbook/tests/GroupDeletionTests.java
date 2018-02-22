33ceeb5ac30dd8c8617627e1e6cd8ad2863a74f6
Zapel <34133527+Zapel@users.noreply.github.com>
2018-02-22 09:48:17 +0200

    lesson49
    улучшаем код  - группы

--- addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/GroupDeletionTests.java
@@ -2,11 +2,19 @@ package net.ukr.ptf.addressbook.tests;
 
 import net.ukr.ptf.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
+    @BeforeMethod
+    public void ensurePreconditions(){
+        app.getNavigationHelper().gotoGroupPage();
+        if(! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+    }
 
     @Test
     public void testGroupDeletion() {
