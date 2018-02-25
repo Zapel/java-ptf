0ecf3a5a78f0bec77352590c88054a0d4adb15b9
Zapel <zapel176@ukr.net>
2018-02-25 05:25:02 +0200

    lesson53
    hamcrest

--- addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,12 @@
 package net.ukr.ptf.addressbook.tests;
 
 import net.ukr.ptf.addressbook.model.ContactData;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
@@ -16,10 +18,10 @@ public class ContactCreationTests extends TestBase {
                 .withFirstName("Zapel").withLastName("Lazeba").withHome("063-798-8633").withGroup("test1");
         app.contact().create(contact, true);
         Set<ContactData> after = app.contact().all();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        assertThat(after.size(), equalTo(before.size() + 1));
 
         contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt());
         before.add(contact);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(before));
     }
 }
