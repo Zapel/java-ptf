415bbb398ae4782f476f0e2910e6276c65f9f8d0
Zapel <zapel176@ukr.net>
2018-03-02 05:57:24 +0200

    home24(lesson62)
    параметризация - контакты

--- addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/net/ukr/ptf/addressbook/tests/ContactCreationTests.java
@@ -2,24 +2,42 @@ package net.ukr.ptf.addressbook.tests;
 
 import net.ukr.ptf.addressbook.model.ContactData;
 import net.ukr.ptf.addressbook.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
         File photo = new File("src/test/resources/28082011(001).jpg");
-        ContactData contact = new ContactData()
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new ContactData()
                 .withFirstName("Oleg").withLastName("Lazeba")
                 .withHomePhone("044-468-9265").withMobilePhone("063-798-8633")
-                .withEmail("zapel176@ukr.net").withGroup("test1").withPhoto(photo);
+                .withEmail("zapel176@ukr.net").withGroup("test1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData()
+                .withFirstName("Oleg").withLastName("Lazeba")
+                .withHomePhone("044-468-9265").withMobilePhone("063-798-8633")
+                .withEmail("zapel176@ukr.net").withGroup("test1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData()
+                .withFirstName("Oleg").withLastName("Lazeba")
+                .withHomePhone("044-468-9265").withMobilePhone("063-798-8633")
+                .withEmail("zapel176@ukr.net").withGroup("test1").withPhoto(photo)});
+        return  list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
+        app.goTo().homePage();
+        Contacts before = app.contact().all();
         app.contact().create(contact, true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
